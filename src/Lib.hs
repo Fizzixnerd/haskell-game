261f343592db485bfe37fc0320a9fcf6436c75fc
Matt Walker <matt.g.d.walker@gmail.com>
2018-02-03 15:24:42 -0500

    Woo!

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,29 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+import ClassyPrelude
+import Graphics.UI.GLFW as GLFW
+import Data.Maybe
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+  GLFW.init
+  mwin <- createWindow 640 640 "Haskell Game Hello World" Nothing Nothing
+  case mwin of
+    Nothing -> error "Could not create window."
+    Just win -> do
+      makeContextCurrent mwin
+      setWindowCloseCallback win (Just $ \w -> setWindowShouldClose w True)
+      listenForEvents win
+  where
+    listenForEvents w = do
+      pollEvents
+      sc <- windowShouldClose w
+      when sc $
+        terminate
+      listenForEvents w
