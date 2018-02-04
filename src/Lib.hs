8100ac5579cd1ce4678b8a52e610df7b05015c0d
Matt Walker <matt.g.d.walker@gmail.com>
2018-02-04 15:10:08 -0500

    Added geometry shader.

--- src/Lib.hs
+++ src/Lib.hs
@@ -47,16 +47,17 @@ makeShader shaderName shaderType = do
 
 compileShaders :: IO G.Program
 compileShaders = do
-
   vertexShader <- makeShader "shader.vs" G.VertexShader
   tessellationControlShader <- makeShader "shader.tcs" G.TessControlShader
   tessellationEvaluationShader <- makeShader "shader.tes" G.TessEvaluationShader
+  geometryShader <- makeShader "shader.gs" G.GeometryShader
   fragmentShader <- makeShader "shader.fs" G.FragmentShader
 
   program <- G.createProgram
   G.attachShader program vertexShader
-  G.attachShader program tessellationControlShader
-  G.attachShader program tessellationEvaluationShader
+--  G.attachShader program tessellationControlShader
+--  G.attachShader program tessellationEvaluationShader
+--  G.attachShader program geometryShader
   G.attachShader program fragmentShader
   G.linkProgram program
   G.validateProgram program
@@ -65,13 +66,14 @@ compileShaders = do
   G.deleteObjectNames [ fragmentShader
                       , tessellationControlShader
                       , tessellationEvaluationShader
+                      , geometryShader
                       , vertexShader ]
   
   return program
 
 render :: G.VertexArrayObject -> G.Program -> IO ()
 render vao p = do
-  G.currentProgram G.$= (Just p)
+  G.currentProgram G.$= Just p
   G.drawArrays G.Triangles 0 3
 
 someFunc :: IO ()
@@ -84,14 +86,14 @@ someFunc = do
         G.setWindowCloseCallback win (Just $ \w -> G.setWindowShouldClose w True)
         prog <- compileShaders
         G.polygonMode G.$= (G.Line, G.Line)
-        G.patchVertices G.$= 3
+        -- G.pointSize G.$= 40.0
 
         vertexArrayObject :: G.VertexArrayObject <- G.genObjectName
         let offsetLocation = G.AttribLocation 0
-            colorLocation = G.AttribLocation 1
+            colorLocation  = G.AttribLocation 1
         G.vertexAttrib4 offsetLocation (0.5 :: Float) 0.5 0.0 0.0
         G.vertexAttrib4 colorLocation (0.8 :: Float) 1.0 0.0 1.0
-        G.bindVertexArrayObject G.$= (Just vertexArrayObject)
+        G.bindVertexArrayObject G.$= Just vertexArrayObject
         G.setWindowRefreshCallback win (Just $ \w -> do
                                            render vertexArrayObject prog
                                            G.swapBuffers w)
