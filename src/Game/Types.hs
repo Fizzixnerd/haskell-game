111b2083db2edfae776e82fd32cb72f8f7a184df
Matt Walker <matt.g.d.walker@gmail.com>
2018-02-06 16:55:45 -0500

    Created NamedHandler and Firable class.

--- src/Game/Types.hs
+++ src/Game/Types.hs
@@ -1,8 +1,17 @@
+{-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module Game.Types where
 
+import ClassyPrelude
 import Control.Lens
+
+import qualified Reactive.Banana.Frameworks as B
 import qualified Linear as L
 
 data Camera = Camera
@@ -11,5 +20,34 @@ data Camera = Camera
   , _cameraFOV :: Float
   }
 
+data NamedHandler a = NamedHandler
+  { _namedHandlerName :: Text
+  , _namedHandlerHandler :: B.Handler a
+  }
 
-mconcat <$> mapM makeLenses [''Camera]
+type NamedEventHandler = (B.AddHandler, NamedHandler)
+
+instance Eq (NamedHandler a) where
+  (NamedHandler { _namedHandlerName = l }) == (NamedHandler { _namedHandlerName = r }) = l == r
+
+instance Ord (NamedHandler a) where
+  compare (NamedHandler { _namedHandlerName = l }) (NamedHandler { _namedHandlerName = r }) = compare l r
+
+instance Show (NamedHandler a) where
+  show (NamedHandler {..}) = unpack _namedHandlerName
+
+class Firable a b where
+  fire :: a -> b -> IO ()
+
+instance a ~ b => Firable (NamedHandler a) b where
+  fire (NamedHandler {..}) b = _namedHandlerHandler b
+
+instance a ~ b => Firable (B.Handler a) b where
+  fire f b = f b
+
+newNamedEventHandler :: Text -> IO (NamedEventHandler a)
+newNamedEventHandler name = do
+  (ah, f) <- B.newAddHandler
+  return (ah, NamedHandler name f)
+
+mconcat <$> mapM makeLenses [''Camera, ''NamedHandler]
