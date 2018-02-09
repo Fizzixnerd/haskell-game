41932eb511df4564a0af1cd4bd9271958a5d7001
Matt Walker <matt.g.d.walker@gmail.com>
2018-02-09 16:53:35 -0500

    Fixed problem with loading plugins and graphics.

--- plugin/src/Plugin/Load.hs
+++ plugin/src/Plugin/Load.hs
@@ -3,6 +3,7 @@
 module Plugin.Load where
 
 import Data.Functor
+import System.Directory
 import GHC
 import GHC.Paths
 import DynFlags
@@ -11,14 +12,16 @@ import Unsafe.Coerce
 type ModName = String
 type ValName = String
 
-loadPlugin :: ModName -> ValName -> IO a
-loadPlugin modName value = 
-  defaultErrorHandler defaultFatalMessager defaultFlushOut $
-  runGhc (Just libdir) $ do
-  dynFlags <- getSessionDynFlags
-  setSessionDynFlags $ dynamicTooMkDynamicDynFlags $ dynFlags { importPaths = [modName] ++ importPaths dynFlags }
-  sequence [guessTarget modName Nothing] >>= setTargets
-  load LoadAllTargets
-  setContext [IIDecl $ simpleImportDecl $ mkModuleName modName]
-  fetched <- compileExpr (modName ++ "." ++ value)
-  return (unsafeCoerce fetched :: a)
+loadPlugin :: FilePath -> ModName -> ValName -> IO a
+loadPlugin dir modName value = do
+  withCurrentDirectory dir $
+    defaultErrorHandler defaultFatalMessager defaultFlushOut $
+    runGhc (Just libdir) $ do
+  
+    dynFlags <- getSessionDynFlags
+    setSessionDynFlags $ dynamicTooMkDynamicDynFlags $ dynFlags { importPaths = [modName] ++ importPaths dynFlags }
+    sequence [guessTarget modName Nothing] >>= setTargets
+    load LoadAllTargets
+    setContext [IIDecl $ simpleImportDecl $ mkModuleName modName]
+    fetched <- compileExpr (modName ++ "." ++ value)
+    return (unsafeCoerce fetched :: a)
