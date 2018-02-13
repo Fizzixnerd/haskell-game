ce2673ef376c321713e74e3558588e773ae5a96a
Matt Walker <matt.g.d.walker@gmail.com>
2018-02-13 18:10:53 -0500

    Add Eq, Ord, and Show instances to Context types.

--- src/Game/Main.hs
+++ src/Game/Main.hs
@@ -93,7 +93,7 @@ doItAndGimmeFireThing = do
 game :: Game ( NamedHandler ()
              , NamedHandler ()
              , IO ()
-             , ThreadId)
+             , ThreadId )
 game = do
   (h, r, sc, w, x) <- doItAndGimmeFireThing
   ti <- liftIO $ forkIO x
