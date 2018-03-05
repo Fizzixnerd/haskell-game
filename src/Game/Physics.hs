1adcaa0d926e84993b32b221cd56ca9b420163d6
Christian Despres <christian.despres1@gmail.com>
2018-03-05 00:09:18 -0500

    Rework foreign objects into foreign resource.

--- src/Game/Physics.hs
+++ src/Game/Physics.hs
@@ -1,92 +1,11 @@
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Game.Physics where
 
 import ClassyPrelude
-import Physics.Bullet
-import Control.Monad.Trans.Resource
-import Data.Acquire
-import Control.Lens
-import Control.Lens.Internal.Setter
+import ForeignResource
 
-type GetM m s a = forall b. (a -> m b) -> (s -> m b)
---type Set' m s a = forall b. (b -> m a) -> (b -> s -> m s)
-type SetM m s a = forall b. (s -> m a) -> (s -> m b) -> (s -> m b)
-type UpdateM m s a = forall b. (a -> m a) -> (s -> m b) -> (s -> m b)
-
-mkGetM :: Monad m => (s -> m a) -> GetM m s a
-mkGetM = (>=>)
-
-mkSetM :: Monad m => (s -> a -> m s) -> SetM m s a
-mkSetM set_ obtain_ f s = obtain_ s >>= set_ s >>= f
-
-mkUpdateM :: Monad m => (s -> m a) -> (s -> a -> m s) -> UpdateM m s a
-mkUpdateM get_ set_ update_ f s = get_ s >>= update_ >>= set_ s >>= f
-
-class ForeignObject a where
-  type ForeignObjectConf a
-  resource :: ForeignObjectConf a -> Acquire a
-
-class Monad m => SettableM m s a t | a -> t where
-  setM :: a -> SetM m s t
-
-class Monad m => GettableM m s a t | a -> t where
-  getM :: a -> GetM m s t
-
-class (Monad m, SettableM m s a t, GettableM m s a t) => UpdatableM m s a t | a -> t where
-  updateM :: a -> UpdateM m s t
-  updateM x = setM x . getM x
-
-(.=!) :: SettableM m s a t => a -> t -> (s -> m b) -> (s -> m b)
-(.=!) targ = setM targ . const . pure
-
-(%=!) :: UpdatableM m s a t => a -> (t -> m t) -> (s -> m b) -> (s -> m b)
-(%=!) = updateM
-
-{-
-(^.!) :: GettableM m s a t => s -> a -> m t
-(^.!)  = flip $ flip getM return
-
-useM :: GettableM m s a t => s -> a -> (t -> m b) -> m b
-useM s a f = s ^.! a >>= f
-
-(&!) :: Monad m => s -> (s -> m b) -> m ()
-s &! f = void . f $ s
--}
-
-
--- a -> f (m a)
--- s -> f (m s)
-type OnlySetter m s a = Setter s (m s) () a
-
-makeOnlySetter :: Monad m => (s -> a -> m s) -> OnlySetter m s a
-makeOnlySetter set_ = taintedDot . flip set_ . flip untaintedDot ()
-
-class (Traversable f, Monad m) => Alien f m where
-  naturalize :: m (f a) -> f (m a)
-
-naturalJoin :: Alien f m => m (f (m a)) -> f (m a)
-naturalJoin = fmap join . naturalize
-
-instance Monad m => Alien Identity m where
-  naturalize = Identity . fmap runIdentity
-
-instance Monad m => Alien (Const (m a)) m where
-  naturalize = Const . join . fmap getConst
-
-type LensM m s t a b = forall f. Alien f m => (a -> f (m b)) -> s -> f (m t)
-
-lensM :: Monad m => (s -> m a) -> (s -> a -> m s) -> LensM m s s a a
-lensM get_ set_ f s = fmap ((set_ s) =<<) $ naturalJoin (f <$> get_ s)
-
-(^.!) :: Monad m => s -> LensM m s s a a -> m a
-(^.!) s l = getConst $ l (Const . return) s
--- |
+-- Example: persistent uniform buffers in OpenGL. They can be attached to binding points (so those binding points are foreign resources to which we can write uniform buffer names), and they have content that we can update.
 
 {--
 {#pointer *action_interface as ^ newtype#}
